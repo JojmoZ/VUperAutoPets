453f54e6615804b83afb574f1d2aeb41a762b72d
JojmoZ <vincentiusjonathantanujaya@gmail.com>
2024-10-02 12:01:47 +0700

    jumbotron text curve

--- home/homepage.js
+++ home/homepage.js
@@ -2,14 +2,10 @@ window.onload = function () {
   console.log("Script Loaded");
   const username = localStorage.getItem("username");
   if (username) {
-    document.querySelector(
-      ".jumbotron h1"
-    ).textContent = `Welcome, ${username}!`;
+    document.getElementById("useruser").textContent =`Welcome ${username}!`;
   } else {
     window.location.href = "/login/start.html";
   }
-
-  // IntersectionObserver for game description
   const gameDescSection = document.querySelector(".game-desc");
   const observer = new IntersectionObserver(
     (entries) => {
@@ -26,8 +22,6 @@ window.onload = function () {
     { threshold: 0.5 }
   );
   observer.observe(gameDescSection);
-
-  // Carousel Logic
   const carouselImages = [
     "../assets/LogoVUPER.jpg",
     "../assets/Steam.png",
@@ -38,26 +32,21 @@ window.onload = function () {
     "This game is available in Steam! Download VUper Auto Pets Free Now!",
     "We are the creators of this game, you can see more of us in https://itch.io/profile/teamwood",
   ];
-
   let currentIndex = 0;
-
   const carouselImageElement = document.getElementById("carousel-image");
   const carouselTextElement = document.getElementById("carousel-text");
   const indicators = document.querySelectorAll(".indicator");
-
   indicators.forEach((indicator) => {
     indicator.addEventListener("click", function () {
       currentIndex = parseInt(this.dataset.index);
       updateCarousel();
     });
   });
-
   function updateCarousel() {
     carouselImageElement.src = carouselImages[currentIndex];
     carouselTextElement.textContent = carouselTexts[currentIndex];
     updateIndicators();
   }
-
   function updateIndicators() {
     indicators.forEach((indicator, index) => {
       if (index === currentIndex) {
@@ -67,16 +56,11 @@ window.onload = function () {
       }
     });
   }
-
-  // Initial update to set the first slide as active
   updateCarousel();
-
-  // Social Media Animation Logic
   const socialMediaSection = document.querySelector(".social-media");
   const instagram = document.querySelector(".instagram");
   const twitter = document.querySelector(".twitter");
   const steam = document.querySelector(".steam");
-
   const socialMediaObserver = new IntersectionObserver(
     (entries) => {
       entries.forEach((entry) => {
@@ -95,6 +79,5 @@ window.onload = function () {
     },
     { threshold: 0.5 }
   );
-
   socialMediaObserver.observe(socialMediaSection);
 };
